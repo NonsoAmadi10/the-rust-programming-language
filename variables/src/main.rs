48a8eb3655365a54ac40e5e4fe5ad327bf92eae3
NonsoAmadi10 <nonsoamadi@aol.com>
2024-03-14 15:22:13 +0100

    chore: add variables and datatypes

---
+++ variables/src/main.rs
@@ -0,0 +1,52 @@
+fn main() {
+    
+    let mut x = 6;
+
+    println!("The value of x is: {x}");
+
+    x = 5;
+    println!("The value of x is: {x}");
+
+    // shadowing allows you to reuse a variable name by assigning it several values
+
+    let x  = 5;
+    let x = x + 1;
+
+    println!("The value of x is : {x}");
+
+    // shadowing is different from mutability in the sense that mutability does not allow for the variable
+    // datatype to change 
+
+    // let mut spaces = "  ";
+    // spaces = spaces.len();
+
+    // println!("spaces is {spaces}")
+
+    // DataTypes
+
+    // Scalar Types: integers, floating point numbers, booleans and characters
+    let u:u32 = 12;
+    println!("The value of u is {u}");
+    let c = 'z';
+
+    println!("The value of c is {c}");
+
+    // Tuples:
+
+    let tup = (500, 6.4, 1);
+
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let b: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = b.0;
+
+    let six_point_four = b.1;
+
+    let one = b.2;
+
+    println!("These are the values of {five_hundred}, {six_point_four}, {one}")
+
+}
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/houdini/Desktop/Projects/learn-rust/variables/target/debug/variables: /home/houdini/Desktop/Projects/learn-rust/variables/src/main.rs
